3c448001b99f9303b499f7dd406140260f6aa9a2
Jason Odoom <jason.odoom@cisa.dhs.gov>
2023-04-19 11:00:08 -0400

    Update flake.nix
    Co-authored-by: dav3r <david.redmin@trio.dhs.gov>

--- flake.nix
+++ flake.nix
@@ -40,7 +40,7 @@
 
             # This hash locks the dependencies of this package. It is
             # necessary because of how Go requires network access to resolve
-            # VCS.  See https://www.tweag.io/blog/2021-03-04-gomod2nix/ for
+            # VCS. See https://www.tweag.io/blog/2021-03-04-gomod2nix/ for
             # details. Normally one can build with a fake sha256 and rely on native Go
             # mechanisms to tell you what the hash should be or determine what
             # it should be "out-of-band" with other tooling (eg. gomod2nix).
