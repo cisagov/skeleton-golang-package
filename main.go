d59fa53410be3d9886097e1fe9b3d55b0d6baabc
Jason Odoom <jason.odoom@cisa.dhs.gov>
2023-06-13 17:19:07 -0400

    Rename pkg to src

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ package main /* The package main tells the Go compiler that the package should
 
 // example is our example Golang package
 import (
-	"example/pkg"
+	"example/src"
 	"fmt"
 	"log"
 	"os"
