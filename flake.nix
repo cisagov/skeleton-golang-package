f5b45096d6d36de0c8d2d2bd8b5a721545988b99
Jason Odoom <jason.odoom@cisa.dhs.gov>
2023-12-22 18:10:09 -0500

    Add nixfmt changes

--- flake.nix
+++ flake.nix
@@ -24,8 +24,7 @@
       # Nixpkgs instantiated for supported system types.
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
 
-    in
-    {
+    in {
 
       # Provide some binary packages for selected system types.
       packages = forAllSystems (system:
